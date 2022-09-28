5c97636dea126e974528e19c3061764cf4293751
koechronix <koechronix@yahoo.com>
2022-09-28 16:23:36 +0300

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :users, only: [:show] do
     resources :items, only: [:show, :index, :create]
-    # resources :users, only: %i[show show create]
+    # resources :items, only: %i[show index create]
   end
 end
